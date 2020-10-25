d20321cc7b08d7a6081bd8b988ffc3620af901a3
maha6802 <magnus.has@gmail.com>
2020-10-25 12:17:46 +0000

    tasks.py updated

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 from celery import Celery
 import os
 
-app = Celery('tasks', broker='amqp://myuser:mypassword@localhost:5672/myvhost')
+app = Celery('tasks', broker='amqp://myuser:mypassword@192.168.2.15:5672/myvhost')
 
 @app.task
 def benchop(prob):
